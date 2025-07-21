7b1997c411e35bb1f21d9b7f445e98b51ac6d988
shu07002 <shu07002@gmail.com>
2025-07-21 15:16:26 +0900

    test: before blog project

--- src/test/java/testexample/jUnitCycleQuiz.java
+++ src/test/java/testexample/jUnitCycleQuiz.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class JUnitCycleQuiz {
+public class jUnitCycleQuiz {
 
     @BeforeEach
     void hello() {
