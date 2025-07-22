3f600c81b3851601834d7f56a7d37f33d8672ec9
shu07002 <shu07002@gmail.com>
2025-07-22 19:39:55 +0900

    etc: add some description about ''@EnableJpaAuditing' and '@EntityListeners'

--- src/main/java/me/kkw/springboot_developer/SpringbootDeveloperApplication.java
+++ src/main/java/me/kkw/springboot_developer/SpringbootDeveloperApplication.java
@@ -4,6 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/*
+* @EnableJpaAuditing
+* 이 애노테이션은 JPA의 Auditing 기능을 전체적으로 활성화하는 스위치와 같은 역할
+* 이 애노테이션을 메인 어플리케이션 클래스나 설정 클래스에 추가하면 Spring Data JPA는
+* 어플리케이션 내에서 Auditing 기능을 사용할 준비를 한다.
+*
+* 다음으로 @EntityListeners(AuditingEntityListener.class)가 붙은 엔티티를 찾아낸다.
+* 그리고 해당 엔티티에 생성이나 수정같은 이벤트가 발생하면 AuditingEntityListener가 동작해서
+* @CreatedDate나 @LastModifiedDate 같은 애노테이션이 붙은 필드에 현재 시간을 자동으로 채운다.
+*
+* createdAt이나 updatedAt 같은 필드를 매번 서비스 로직에서 LocalDateTime.now() 등으로 직접 설정해 줄 필요가 없다.
+*/
 @EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootDeveloperApplication {
--- src/main/java/me/kkw/springboot_developer/domain/Article.java
+++ src/main/java/me/kkw/springboot_developer/domain/Article.java
@@ -18,6 +18,21 @@ import java.time.LocalDateTime;
 * 실제 데이터 원본 그 자체다
 */
 
+
+/*
+* @EntityListeners는 JPA의 Auditing 기능 사용하려고 붙은 애노테이션
+* 앤티티가 생성되거나 수정될 때 특정 동작을 자동으로 수행하도록 설정
+* 여기서는 AuditingEntityListener.class를 지정 -> Spring Data JPA가 제공하는
+* Auditing 리스너 사용
+* 이 리스너 사용하면 Article 클래스 내의 특정 필드에 있는 애노테이션 찾아서 동작
+* 여기 밑에는 @CreatedDate, @LastModifiedDate가 붙은 메소드들이 있음
+* AuditingEntityListener는 이 애노테이션을 감지하여 아래 작업을 자동으로 처리
+* 1. 앤티티 생성 시 @CreatedDate가 붙은 createdAt 필드에 현재 시간을 자동으로 저장
+* 2. 엔티티 수정 시 @LastModified가 붙은 updatedAt 필드에 현재 시간을 자동으로 저장
+*
+* 이 기능이 제대로 동작하려면 메인 어플리케이션 클래스에
+* @EnableJpaAuditing 애노테이션을 추가해야함
+*/
 @EntityListeners(AuditingEntityListener.class)
 @Entity // 엔티티로 지정
 @Getter // 클래스의 모든 필드에 대한 접근자 메소드를 만듦
